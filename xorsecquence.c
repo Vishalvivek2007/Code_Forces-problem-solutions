2febe04ec8aa1e7c3c6aaf02259077d58f13a9ba
Vishal Vivek <vishal.vivek.10039@gmail.com>
2025-09-07 16:36:43 +0530

    xor sequence question code forces (1000 rating)

---
+++ xorsecquence.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+  int n;
+  scanf("%d",&n);
+  while(n--){
+    long long a ,b;
+    scanf("%lld %lld",&a,&b);
+    for(long long i=0;i<50;i++){
+      if((a&(1ll << i)) != (b&(1ll << i))){
+        printf("%lld\n",1ll <<i);
+        break;
+      }
+    }
+  }
+}
