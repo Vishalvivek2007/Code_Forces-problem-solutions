c6e64e22a343e624abda8ef25ef6aadbf7232ad9
Vishal Vivek <vishal.vivek.10039@gmail.com>
2025-09-16 21:20:02 +0530

    changing formating

--- xorsecquence.c
+++ xorsecquence.c
@@ -8,8 +8,10 @@ int main(){
     scanf("%lld %lld",&a,&b);
     for(long long i=0;i<50;i++){
       if((a&(1ll << i)) != (b&(1ll << i))){
+        
         printf("%lld\n",1ll <<i);
         break;
+
       }
     }
   }
